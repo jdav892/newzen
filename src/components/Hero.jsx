034501ea29cb7edcd28195a5431e4bdd5cfb2a22
jdav892 <jadavila9@gmail.com>
2024-12-05 18:42:13 -0500

    feat: steps through all videos for display on click through while also incrementing the background scene to alternate visuals

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,7 +35,7 @@ const Hero = () => {
                   <div onClick={handleMiniVideoClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
                       <video
                         ref={nextVideoRef}
-                        src={getVideoSrc(currentIndex + 1)}
+                        src={getVideoSrc(upcomingVideoIndex)}
                         loop
                         muted
                         id="current-video"
@@ -44,6 +44,10 @@ const Hero = () => {
                         /> 
                     </div> 
                 </div>
+                <video 
+                  ref={nextVideoRef}
+                  src={getVideoSrc(currentIndex)}
+                  />
             </div>
         </div>    
     </div>
