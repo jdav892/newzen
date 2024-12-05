4371f97329799714148e9341292e991d3dd899ac
jdav892 <jadavila9@gmail.com>
2024-12-05 18:32:36 -0500

    feat: video loads on hover. todo: need to fix video looping

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,13 @@ const Hero = () => {
     setCurrentIndex((prevIndex) => prevIndex + 1);
   }
 
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);   
+  }
+
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`
+
+ 
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
@@ -23,9 +29,15 @@ const Hero = () => {
         rounded-lg bg-blue-75">
             <div>
                 <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-                  <div onClick={handleMiniVideoClick} className="origin-center">
+                  <div onClick={handleMiniVideoClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
                       <video
                         ref={nextVideoRef}
+                        src={getVideoSrc(currentIndex + 1)}
+                        loop
+                        muted
+                        id="current-video"
+                        className="size-64 origin-center scale-150 object-cover object-center"
+                        onLoadedData={handleVideoLoad}
                         /> 
                     </div> 
                 </div>
