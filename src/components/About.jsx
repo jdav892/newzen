0035c673b85850ce7ff8a1405abf227ad10aaf68
jdav892 <jadavila9@gmail.com>
2024-12-07 16:13:00 -0500

    feat: about section animates on scroll using gsap

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from "gsap"
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const About = () => {
+
+    useGSAP(() => {
+        const clipAnimation = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#clip',
+                start: 'center center',
+                end: '+=800 center',
+                scrub: 0.5,
+                pin: true,
+                pinSpacing: true,
+            }
+        })
+
+        clipAnimation.to('.mask-clip-path', {
+            width: '100vw',
+            height: '100vh',
+            borderRadius: 0
+        })
+    })
+
   return (
     <div id="about" className="min-h-screen w-screen">
         <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
@@ -8,9 +32,26 @@ const About = () => {
                 Welcome to Zentry
             </h2>
             <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-                    Discover the World's largest shared adventure
+                    Disc<b>o</b>ver the World's <br /> l<b>a</b>rgest shared adventure
+            </div>
+            <div className="about-subtext">
+                <p>
+                    The Game of Games begins-your life, now an epic MMORPG
+                </p>
+                <p>
+                    Zentry unites every player from countless games and platforms
+                </p>
             </div>
         </div>
+            <div className="h-dvh w-screen" id="clip">
+                <div className="mask-clip-path about-image">
+                    <img 
+                        src="img/about.webp"
+                        alt="Background"
+                        className="absolute left-0 top-0 size-full object-cover"
+                    />
+                </div>
+            </div>
     </div>
   )
 }
