27e6f7c026cfbc7434daa8f45f0eff8ee757d795
jdav892 <jadavila9@gmail.com>
2024-12-07 16:03:18 -0500

    feat: adds about section with some text that will be iterated on

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id="about" className="min-h-screen w-screen">
+        <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+            <h2 className="font-general text-sm uppercase md:text-[10px]">
+                Welcome to Zentry
+            </h2>
+            <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+                    Discover the World's largest shared adventure
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
