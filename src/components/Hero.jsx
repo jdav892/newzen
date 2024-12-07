42104d28b70a42ae9299832579e5ec6761e0bb41
jdav892 <jadavila9@gmail.com>
2024-12-07 00:44:38 -0500

    feat: changes size of header video on scroll using scroll trigger from gsap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,35 +1,43 @@
-import React from 'react'
-import {useState, useRef} from 'react';
+import {useState, useRef, useEffect} from 'react';
 import { TiLocationArrow } from 'react-icons/ti';
 import Button from './Button'
 import { useGSAP } from "@gsap/react"
 import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
 
   const [currentIndex, setCurrentIndex] = useState(1);
   const [hasClicked, setHasClicked] = useState(false);
+
   const [isLoading, setIsLoading] = useState(true);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
   const totalVideos = 4;
-  const nextVideoRef = useRef();
+  const nextVideoRef = useRef(null);
 
   const handleVideoLoad = () => {
     setLoadedVideos((prev) => prev + 1);   
   }
 
-  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+  useEffect(() => {
+    if(loadedVideos === totalVideos - 1){
+      setIsLoading(false);
+    }
+  }, [loadedVideos]);
 
+  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+  
   const handleMiniVideoClick = () => {
     setHasClicked(true);
-    setCurrentIndex(upcomingVideoIndex);
+    setCurrentIndex((upcomingVideoIndex));
   }
 
   useGSAP(() => {
     if(hasClicked) {
       gsap.set('#next-video', {visibility: 'visible'});
-
       gsap.to('#next-video', {
         transformOrigin: 'center center',
         scale: 1,
@@ -39,7 +47,6 @@ const Hero = () => {
         ease: 'power1.inOut',
         onStart: () => nextVideoRef.current.play(),
       });
-
       gsap.from('#current-video', {
         transformOrigin: 'center center',
         scale: 0,
@@ -47,20 +54,24 @@ const Hero = () => {
         ease: 'power1.inOut',
       })
     }
-  }, {dependencies: [currentIndex], revertOnUpdate: true})
+  }, 
+    {
+      dependencies: [currentIndex], 
+      revertOnUpdate: true
+    }
+  );
  
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
   useGSAP(() => {
     gsap.set('#video-frame', {
       clipPath: 'polygon(14% 0%, 72% 0%, 88% 90%, 0% 95%)',
-      borderRadius: '0% 0% 40% 10%'
-    })
-
+      borderRadius: '0% 0% 40% 10%',
+    });
     gsap.from('#video-frame', {
-      clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%);',
+      clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
       borderRadius: '0 0 0 0',
-      ease: 'power.inOut',
+      ease: 'power1.inOut',
       scrollTrigger: {
         trigger: '#video-frame',
         start: 'center center',
@@ -72,8 +83,9 @@ const Hero = () => {
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
+
       {isLoading && (
-        <div>
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
           <div className="three-body">
             <div className="three-body__dot"></div>
             <div className="three-body__dot"></div>
@@ -81,6 +93,7 @@ const Hero = () => {
           </div>
         </div>
       )}
+
         <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden
         rounded-lg bg-blue-75">
             <div>
@@ -112,6 +125,7 @@ const Hero = () => {
                   loop
                   muted
                   className="absolute left-0 top-0 size-full object-cover object-center"
+                  onLoadedData={handleVideoLoad}
                 />
             </div>
             <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
@@ -137,4 +151,4 @@ const Hero = () => {
   )
 }
 
-export default Hero
+export default Hero;
