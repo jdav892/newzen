b8be94d4667d660c9ebc6cecbf655e68dd103006
jdav892 <jadavila9@gmail.com>
2024-12-07 17:55:26 -0500

    feat: creates an animated title component that can be reused

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import { useGSAP } from '@gsap/react'
 import gsap from "gsap"
 import { ScrollTrigger } from 'gsap/all'
+import AnimatedTitle from './AnimatedTitle'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -31,9 +32,7 @@ const About = () => {
             <h2 className="font-general text-sm uppercase md:text-[10px]">
                 Welcome to Zentry
             </h2>
-            <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-                    Disc<b>o</b>ver the World's <br /> l<b>a</b>rgest shared adventure
-            </div>
+                <AnimatedTitle />
             <div className="about-subtext">
                 <p>
                     The Game of Games begins-your life, now an epic MMORPG
