a3ba9a1177dc0a85c54e6de96fb96185cb85dcc0
jdav892 <jadavila9@gmail.com>
2024-12-12 16:18:53 -0500

    feat: animates story text as well as render image with change of color of title in parts under image

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import AnimatedTitle from "./AnimatedTitle";
 
 const Story = () => {
   return (
     <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
-        Story
+      <div className="flex size-full flex-col items-center py-10 pb-25">
+        <p className="font-general text-sm uppercase md:text-[10px]">the multiversal ip world</p>
+        <div className="relative size-full">
+          <AnimatedTitle 
+            title="The st<b>o</b>ry of<br/> a hidden real<b>m</b>"
+            sectionId="#story"
+            containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10" 
+          />
+          <div className="story-img-container">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <img
+                  src="/img/entrance.webp"
+                  alt="entrance"
+                  className="object-contain"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
