a0aa5862f6ff880eb45200fa86fa9b1fab8240d2
jdav892 <jadavila9@gmail.com>
2024-12-12 16:55:48 -0500

    feat: animates the image that we mouseover with color change based on overlap of image and title text

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,13 +1,46 @@
 import AnimatedTitle from "./AnimatedTitle";
+import React, { useRef } from "react"
+import gsap from 'gsap'
 
 const Story = () => {
+  const frameRef = useRef('null');
+
+  const handleMouseLeave = () => {
+
+  }
+  
+    const handleMouseMove = (i) => {
+    const {clientX, clientY} = i;
+    const element = frameRef.current;
+    
+    if(!element) return;
+
+    const rect = element.getBoundingClientRect();
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    
+    const rotateX = ((y - centerY) / centerY) * -10;
+    const rotateY = ((x - centerX) / centerX) * 10;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX, rotateY,
+      transformPerspective: 500,
+      ease: 'power1.inOut',
+    })
+
+  }
+
   return (
     <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
       <div className="flex size-full flex-col items-center py-10 pb-25">
         <p className="font-general text-sm uppercase md:text-[10px]">the multiversal ip world</p>
         <div className="relative size-full">
           <AnimatedTitle 
-            title="The st<b>o</b>ry of<br/> a hidden real<b>m</b>"
+            title="The st<b>o</b>ry of <br/> a hidden real<b>m</b>"
             sectionId="#story"
             containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10" 
           />
@@ -15,6 +48,11 @@ const Story = () => {
             <div className="story-img-mask">
               <div className="story-img-content">
                 <img
+                  ref={frameRef}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  onMouseMove={handleMouseMove}
                   src="/img/entrance.webp"
                   alt="entrance"
                   className="object-contain"
